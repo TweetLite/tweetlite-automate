b9fd909cde0931c0ea5cf45ccd0b1fd8b0e7fea3
ayhankuru <ayhankuru@yandex.com.tr>
2017-04-23 17:03:56 +0300

    updated

--- index.js
+++ index.js
@@ -1,49 +1,80 @@
-var dParser = require('duration-parser')
-
 exports.use = function () {
 	return function (args) {
-		var time = dParser(args.interval) || dParser('30 minutes')
-		if (time < 900000) {
-			throw new Error('Time must be 15 minutes later')
-		} else if (args.message) { // eslint-disable-line no-negated-condition
-			var mesaj = null
-			if (typeof args.message === 'object') {
-				mesaj = args.message.join('\n').replace(/\\n/g, '\n')
-			} else {
-				mesaj = args.message.replace(/\\n/g, '\n')
+		this.use({
+			welcomeMessageCreate: {
+				path: 'direct_messages/welcome_messages/new',
+				method: 'post'
+			},
+			welcomeMessageDelete: {
+				path: 'direct_messages/welcome_messages/destroy',
+				method: 'delete'
+			},
+			welcomeMessageList: {
+				path: 'direct_messages/welcome_messages/list',
+				method: 'get'
+			},
+			welcomeMessageShow: {
+				path: 'direct_messages/welcome_messages/show',
+				method: 'get'
+			},
+			welcomeMessageRulesList :{
+				path: 'direct_messages/welcome_messages/rules/list',
+				method: 'get'
+			},
+			welcomeMessageRule :{
+				path: 'direct_messages/welcome_messages/rules/new',
+				method: 'post'
 			}
-			automate(this, time, mesaj)
+		});
+
+
+		var mesaj = null
+		if (typeof args.message === 'object') {
+			mesaj = args.message.join('\n').replace(/\\n/g, '\n')
 		} else {
-			throw new Error('Message must be set')
+			mesaj = args.message.replace(/\\n/g, '\n')
 		}
+			
+		this.welcomeMessageList().then( res => {
+			if(res.welcome_messages){
+				Promise.all(res.welcome_messages.map(message => {
+					return this.welcomeMessageDelete({
+							id: message.id
+							});
+				})).then(result => {
+
+					console.log(result);
+
+				}).catch(err => console.log(err));
+			}
+
+			this.welcomeMessageCreate({
+			  "welcome_message" : {
+			  		"message_data": {
+			      		"text": mesaj
+			   		}
+				}
+			}).then( res => {
+						
+				if(res.welcome_message){
+
+					var id = res.welcome_message.id;
+						this.welcomeMessageRule({
+						  "welcome_message_rule": {
+					      "welcome_message_id": id
+					       }
+						}).then( res => {
+							if(res.welcome_message_rule){
+								console.log('Welcome Message Activated!')
+							}
+						}).catch(err => console.log(err));
+
+				} else {
+					console.log('Welcome Message Not Created!')
+				}
+			}).catch(err => console.log(err));
+
+		}).catch(err => console.log(err));
+
 	}
-}
-
-function automate(context, time, msg, data) {
-	context.extra.fullFollowers().then(result => {
-		if (data) {
-			console.log(data.length, result.length)
-			var check = arrDiff(result, data)
-			if (check.length <= 0) {
-				setTimeout(function () {
-					automate(context, time, msg, result)
-				}, time)
-			} else {
-				context.extra.fullUserMessage(check, msg).then(() => {
-					automate(context, time, msg, result)
-				}).catch(() => {
-					automate(context, time, msg, result)
-				})
-			}
-		} else {
-			automate(context, time, msg, result)
-		}
-	}).catch(err => {
-		console.log(err)
-		process.exit() // eslint-disable-line xo/no-process-exit
-	})
-}
-
-function arrDiff(arr1, arr2) {
-	return arr1.filter(x => arr2.indexOf(x) === -1)
 }
